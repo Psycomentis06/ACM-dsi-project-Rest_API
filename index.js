c11ead404efdf8910b6631d24fed77a74175b3ec
Amor Ali <alibenamor30@gmail.com>
2020-11-17 21:27:12 +0100

    Getting started

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var admin = require('firebase-admin');
+var PORT = process.env.PORT || 3000;
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// Firebase init
+var serviceAccount = require('./dsi3-project-firebase-adminsdk-rl8cr-9e76e0fea6.json');
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://dsi3-project.firebaseio.com/"
+})
+// Routes
+app.get('/', (req, res) =>{
+    var sql = require('./libs/db');
+
+    sql.query('SELECT * FROM user', (error, result, field) => {
+        if (error) {
+            return console.log(error.message);
+        }
+        res.json({
+            data: result
+        })
+    })
+})
+
+app.listen(PORT, () => {
+    console.log("Listen to localhost:"+PORT);
+})
---
+++ readme.md
@@ -0,0 +1,15 @@
+### ACM DSI3 Project Backend ###
+
+The structure is simple for the backend (MVC architecture based).
+
+    - `models | controllers` : MVC.
+
+    - `tests` : Where you can test some DB functions before implementing them to the app.
+
+    - `libs` : For some configurations like mysql connection.
+
+    - `errors` : Custom error classes.
+
+**Setting up the project**
+
+You have just to add mysql server cridentials and firebase api key json file in root folder
