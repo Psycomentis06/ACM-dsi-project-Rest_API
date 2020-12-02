c1842aba09b1bbf9a581a16c763960b3b8a9761f
Amor Ali <alibenamor30@gmail.com>
2020-12-02 16:31:41 +0100

    local port changed to 4000

--- index.js
+++ index.js
@@ -1,38 +1,37 @@
 // imports
-var express = require('express');
+var express = require("express");
 var app = express();
-var admin = require('firebase-admin');
-var cors = require('cors');
+var admin = require("firebase-admin");
+var cors = require("cors");
 // Constants
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 4000;
 var API_BASE_URI = "/api/v1/";
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(function(err, req, res, next) {
-
-  // error 
+app.use(function (err, req, res, next) {
+  // error
   res.status(500).json({
-      valid: false,
-      message: "Error in body parsing or maybe another problem. please check your body request JSON format or contact us for this bug"
+    valid: false,
+    message:
+      "Error in body parsing or maybe another problem. please check your body request JSON format or contact us for this bug",
   });
-
 });
 // Firebase init
-var serviceAccount = require('./dsi3-project-firebase-adminsdk-rl8cr-9e76e0fea6.json');
+var serviceAccount = require("./dsi3-project-firebase-adminsdk-rl8cr-9e76e0fea6.json");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://dsi3-project.firebaseio.com/"
-})
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://dsi3-project.firebaseio.com/",
+});
 // Routes
-app.use('/', express.static('public')) // Documentation page
-app.get('/', (req, res) => {
-    res.send('index.html');
-})
-const userRoutes = require('./routes/user');
-app.use(API_BASE_URI+"user", userRoutes);
+app.use("/", express.static("public")); // Documentation page
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+const userRoutes = require("./routes/user");
+app.use(API_BASE_URI + "user", userRoutes);
 
 app.listen(PORT, () => {
-    console.log("Listen to localhost:"+PORT);
-})
+  console.log("Listen to localhost:" + PORT);
+});
