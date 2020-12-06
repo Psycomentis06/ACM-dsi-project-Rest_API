b6c6ff1027b975939900e64169f81332a27447b7
Amor Ali <alibenamor30@gmail.com>
2020-12-06 20:17:42 +0100

    empty

---
+++ libs/acm.js
@@ -0,0 +1,27 @@
+// small module contain some utility functions like empty isset
+
+function empty(value) {
+  let undef;
+  let key;
+  let i;
+  let len;
+  const emptyValues = [undef, null, false, 0, "", "0"];
+  for (i = 0, len = emptyValues.length; i < len; i++) {
+    if (mixedVar === emptyValues[i]) {
+      return true;
+    }
+  }
+  if (typeof mixedVar === "object") {
+    for (key in mixedVar) {
+      if (mixedVar.hasOwnProperty(key)) {
+        return false;
+      }
+    }
+    return true;
+  }
+  return false;
+}
+
+module.exports = {
+  empty,
+};
