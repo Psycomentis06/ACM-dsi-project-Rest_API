fa7bec527f0a25ec0fbb2f5d02394723118ec078
Amor Ali <alibenamor30@gmail.com>
2020-12-14 22:39:00 +0100

    product sync

--- libs/db_sync.js
+++ libs/db_sync.js
@@ -1,4 +1,5 @@
 // Sync database
-const User = require('../models/user');
-
-User.sync({alter: true});
+const User = require("../models/user");
+const Product = require("../models/product");
+User.sync({ alter: true });
+Product.sync({ alter: true });
