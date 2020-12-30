b82949f781e43306994c3ff29e7454dd27199490
Amor Ali <alibenamor30@gmail.com>
2020-12-30 22:08:52 +0100

    Gmail smtp

--- mailer.js
+++ mailer.js
@@ -1,29 +1,30 @@
-const nodemailer = require('nodemailer');
-const hbs = require('nodemailer-express-handlebars');
-const path = require('path');
+const nodemailer = require("nodemailer");
+const hbs = require("nodemailer-express-handlebars");
+const path = require("path");
 var transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
-    secure: false,
-    auth: {
-        user: "apikey",
-        pass: "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"
-    },
-    tls: {
-        rejectUnauthorized: false
-    }
-})
+  service: "Gmail",
+  auth: {
+    user: "dsi3.acm@gmail.com",
+    pass: "392817654",
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
 
 // template engine middleware
 
-transporter.use('compile', hbs({
+transporter.use(
+  "compile",
+  hbs({
     viewEngine: {
-        extname: 'hbs', // handlebars extension
-        layoutsDir: 'templates', // location of handlebars templates
-        defaultLayout: false, // name of main template
+      extname: "hbs", // handlebars extension
+      layoutsDir: "templates", // location of handlebars templates
+      defaultLayout: false, // name of main template
     },
     viewPath: "templates",
-    extName: '.hbs'
-}))
+    extName: ".hbs",
+  })
+);
 
 module.exports = transporter;
