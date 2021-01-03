bdedf515db54545125252d27481ee6b0d348816e
Amor Ali <alibenamor30@gmail.com>
2021-01-03 22:10:08 +0100

    routes

---
+++ routes/order.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const orderController = require("../controllers/ordre");
+const adminAuth = require("../middlewares/admin.auth");
+
+/**
+ * Get all orders
+ */
+router.get("/all", adminAuth, (req, res) =>
+  orderController.getOrders(req, res)
+);
+
+/**
+ * Get order by user_id
+ */
+router.get("/:id", adminAuth, (req, res) => orderController.getOrder(req, res));
+
+/**
+ * Edit order
+ */
+
+router.put("/:id", adminAuth, (req, res) =>
+  orderController.editOrder(req, res)
+);
