fe3b129165e1fbf62d446411ebef98202b56f6ad
Amor Ali <alibenamor30@gmail.com>
2021-01-11 18:43:59 +0100

    Set user offline if auth error

--- middlewares/admin.auth.js
+++ middlewares/admin.auth.js
@@ -1,27 +1,30 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-    // Middleware for normal user auth
-    try {
-        const whiteRoles = ['ROLE_ADMIN', 'ROLE_SUPERADMIN'];
-        tokenHeader = req.header('Authorization').split(' ')[1];
-        const tokenData = jwt.verify(tokenHeader, process.env.JWT_KEY);
-        // check permission
-        if (whiteRoles.includes(tokenData.role)) {
-            // valid permission
-            req.body.tokenData = tokenData;
-            next();
-        } else {
-            res.status(401).json({
-                valid: false,
-                message: 'Wrong privileges'
-            });
-        }
-
-    } catch (err) {
-        res.status(401).json({
-            valid: false,
-            message: "Auth error"
-        });
+  // Middleware for normal user auth
+  const tokenData = {};
+  try {
+    const whiteRoles = ["ROLE_ADMIN", "ROLE_SUPERADMIN"];
+    tokenHeader = req.header("Authorization").split(" ")[1];
+    tokenData = jwt.verify(tokenHeader, process.env.JWT_KEY);
+    // check permission
+    if (whiteRoles.includes(tokenData.role)) {
+      // valid permission
+      req.body.tokenData = tokenData;
+      next();
+    } else {
+      res.status(401).json({
+        valid: false,
+        message: "Wrong privileges",
+      });
     }
-}
+  } catch (err) {
+    req.body.status = "offline";
+    req.params.id = tokenData.id;
+    require("../controllers/user").setStatus(req, res); // set user to offline
+    res.status(401).json({
+      valid: false,
+      message: "Auth error",
+    });
+  }
+};
--- middlewares/user.auth.js
+++ middlewares/user.auth.js
@@ -2,10 +2,11 @@ const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
   // Middleware for normal user auth
+  const tokenData = {};
   try {
     const whiteRoles = ["ROLE_USER", "ROLE_ADMIN", "ROLE_SUPERADMIN"];
     tokenHeader = req.header("Authorization").split(" ")[1];
-    const tokenData = jwt.verify(tokenHeader, process.env.JWT_KEY);
+    tokenData = jwt.verify(tokenHeader, process.env.JWT_KEY);
     // check permission
     if (whiteRoles.includes(tokenData.role)) {
       // valid permission
@@ -18,6 +19,9 @@ module.exports = (req, res, next) => {
       });
     }
   } catch (err) {
+    req.body.status = "offline";
+    req.params.id = tokenData.id;
+    require("../controllers/user").setStatus(req, res); // set user to offline
     res.status(401).json({
       valid: false,
       message: "Auth error",
